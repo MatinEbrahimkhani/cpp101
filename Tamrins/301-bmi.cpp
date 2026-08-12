63d94e3a8082e61e85e3e10597309df8d29dcd44
Matin <mmgn46@yahoo.com>
2026-08-12 13:39:42 +0330

    adding 301 Exercise

---
+++ Tamrins/301-bmi.cpp
@@ -0,0 +1,22 @@
+/*
+salam amirreza
+
+*/
+
+#include <iostream>
+using namespace std;
+double BMI(int weight, int height){
+ //TODO: 
+}
+
+int main(){
+    int height, weight;
+    cout << "Enter your Height:";
+    cin >> height;
+
+    cout << "Enter your Weight:";
+    cin >> weight;
+
+    double bmi = BMI(height, weight);
+    return 0;
+}
