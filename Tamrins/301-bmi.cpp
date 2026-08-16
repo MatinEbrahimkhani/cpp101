d3027db234144dafbdeaa8d2a4d2c5026bf27d30
amir <amir.catalanii@gmail.com>
2026-08-16 13:27:53 +0430

    301.bmi.cpp

--- Tamrins/301-bmi.cpp
+++ Tamrins/301-bmi.cpp
@@ -44,17 +44,18 @@
 #include <iostream>
 using namespace std;
 double BMI(int weight, int height){
- //TODO: 
+  double  bmi = ((height * height) / weight);
+  return bmi;
+
 }
 
 int main(){
     int height, weight;
     cout << "Enter your Height:";
     cin >> height;
-
     cout << "Enter your Weight:";
-    cin >> weight;
-
+    cin >> weight;  
     double bmi = BMI(height, weight);
+    cout << bmi;
     return 0;
 }
