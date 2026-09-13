a0481702e2c4ca3380306da113ab4ff26881bb01
Matin <mmgn46@yahoo.com>
2026-09-13 14:02:21 +0330

    adding factorial to functions

--- Courses/06_Functions.cpp
+++ Courses/06_Functions.cpp
@@ -7,42 +7,71 @@
 // ============================================================
 
 #include <iostream>
+using namespace std;
 
 // --- Example 1: a function that returns a value ---
-int square(int x) {
+int square(int x)
+{
     return x * x;
 }
 
 // --- Example 2: a function with two parameters ---
-int add(int a, int b) {
+int add(int a, int b)
+{
     return a + b;
 }
 
 // --- Example 3: a void function - does something, returns nothing ---
-void printLine() {
-    std::cout << "------------------------" << std::endl;
+void printLine()
+{
+    cout << "------------------------" << endl;
 }
 
 // --- Example 4: a function that calls another function ---
-bool isPrime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return false;
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
     }
     return true;
 }
 
-int main() {
-    int num = 4;
-    std::cout << "Square of " << num << " is " << square(num) << std::endl;
-    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
+int cube(int x)
+{
+    int result = x * x * x;
+    return result;
+}
+int factorial(int n)
+{
+    int result = n;
+    for (int i = n - 1; i > 0; i--)
+    {
+        result = result * i; // result*=i
+    }
 
-    printLine();
+    return result;
+}
 
-    for (int i = 2; i <= 20; i++) {
-        if (isPrime(i)) {
-            std::cout << i << " is prime" << std::endl;
-        }
+int main()
+{
+    // int num = 4;
+    // cout << "Square of " << num << " is " << square(num) << endl;
+    // cout << "5 + 3 = " << add(5, 3) << endl;
+
+    // printLine();
+
+    // for (int i = 2; i <= 20; i++) {
+    //     if (isPrime(i)) {
+    //         cout << i << " is prime" << endl;
+    //     }
+    // }
+    for (int i = 1; i < 10; i++)
+    {
+        cout << "Factorial {"<<i<<"}=" << factorial(i) << endl;
     }
 
     return 0;
